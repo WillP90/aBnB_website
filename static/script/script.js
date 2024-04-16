64d19e08d18f44f4f9f64dbf701b519b20d068d1
william pearce <william.pearce000@gmail.com>
2024-04-15 20:02:41 -0700

    more

--- static/script/script.js
+++ static/script/script.js
@@ -1,20 +1,31 @@
+import {
+    clearAllBodyScrollLocks,
+    disableBodyScroll,
+    enableBodyScroll,
+} from "https://cdn.jsdelivr.net/gh/rick-liruixin/body-scroll-lock-upgrade@v1.0.3.1/lib/index.esm.js";
+
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
 const media = window.matchMedia('(width < 40em)');
 const topNavMenu = document.querySelector('.hamburger')
 const main = document.querySelector('main')
+const body = document.querySelector('body')
 
 function openHamburger(){
     btnOpen.setAttribute('aria-expanded', 'true');
     topNavMenu.removeAttribute('inert');
     topNavMenu.removeAttribute('style');
     main.setAttribute('inert', '');
+    bodyScrollLockUpgrade.disableBodyScroll(body);
+    btnClose.focus();
 }
 
 function closeHamburger(){
     btnOpen.setAttribute('aria-expanded', 'false');
     topNavMenu.setAttribute('inert', '');
     main.removeAttribute('inert');
+    bodyScrollLockUpgrade.enableBodyScroll(body);
+    btnOpen.focus();
 
     setTimeout(() => {
         topNavMenu.style.transition = 'none';
@@ -30,6 +41,7 @@ function setupTopNav(e){
     else{
         console.log('desktop view');
         topNavMenu.removeAttribute('inert')
+        closeHamburger();
     }
 }
 setupTopNav(media);
