66bd8a9c76eaab1310aebac3be14c34b8b85dceb
william pearce <william.pearce000@gmail.com>
2024-04-15 18:59:26 -0700

    more

--- static/script/script.js
+++ static/script/script.js
@@ -1,13 +1,42 @@
-const btnOpen = document.querySelector('#btnOpen')
-const btnClose = document.querySelector('#btnClose')
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const media = window.matchMedia('(width < 40em)');
+const topNavMenu = document.querySelector('.hamburger')
+const main = document.querySelector('main')
 
 function openHamburger(){
     btnOpen.setAttribute('aria-expanded', 'true');
+    topNavMenu.removeAttribute('inert');
+    topNavMenu.removeAttribute('style');
+    main.setAttribute('inert', '');
 }
 
 function closeHamburger(){
-    btnClose.setAttribute('aria-expanded', 'false');
+    btnOpen.setAttribute('aria-expanded', 'false');
+    topNavMenu.setAttribute('inert', '');
+    main.removeAttribute('inert');
+
+    setTimeout(() => {
+        topNavMenu.style.transition = 'none';
+    }, 500);
 }
+
+function setupTopNav(e){
+    if(e.matches){
+        console.log('mobile view');
+        topNavMenu.setAttribute('inert', '');
+        topNavMenu.style.transition = 'none';
+    }
+    else{
+        console.log('desktop view');
+        topNavMenu.removeAttribute('inert')
+    }
+}
+setupTopNav(media);
 
 btnOpen.addEventListener('click', openHamburger);
 btnClose.addEventListener('click', closeHamburger);
+
+media.addEventListener('change', function(e){
+    setupTopNav(e);
+});
