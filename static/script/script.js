4bc38944ba440387f87de7f7c3976247c27fd974
william pearce <william.pearce000@gmail.com>
2024-04-20 07:55:58 -0700

    bug fixes

--- static/script/script.js
+++ static/script/script.js
@@ -1,17 +1,11 @@
-import {
-    clearAllBodyScrollLocks,
-    disableBodyScroll,
-    enableBodyScroll,
-} from "https://cdn.jsdelivr.net/gh/rick-liruixin/body-scroll-lock-upgrade@v1.0.3.1/lib/index.esm.js";
-
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
 const media = window.matchMedia('(width < 40em)');
-const topNavMenu = document.querySelector('.hamburger')
-const main = document.querySelector('main')
-const body = document.querySelector('body')
+const topNavMenu = document.querySelector('.hamburger');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
 
-function openHamburger(){
+function openMobileMenu() {
     btnOpen.setAttribute('aria-expanded', 'true');
     topNavMenu.removeAttribute('inert');
     topNavMenu.removeAttribute('style');
@@ -20,7 +14,7 @@ function openHamburger(){
     btnClose.focus();
 }
 
-function closeHamburger(){
+function closeMobileMenu() {
     btnOpen.setAttribute('aria-expanded', 'false');
     topNavMenu.setAttribute('inert', '');
     main.removeAttribute('inert');
@@ -32,23 +26,25 @@ function closeHamburger(){
     }, 500);
 }
 
-function setupTopNav(e){
-    if(e.matches){
-        console.log('mobile view');
-        topNavMenu.setAttribute('inert', '');
+function setupTopNav(e) {
+    if (e.matches) {
+        // is mobile
+        console.log('is mobile');
         topNavMenu.style.transition = 'none';
-    }
-    else{
-        console.log('desktop view');
-        topNavMenu.removeAttribute('inert')
-        closeHamburger();
+        topNavMenu.setAttribute('inert', '');
+    } else {
+        // is tablet/desktop
+        console.log('is desktop');
+        closeMobileMenu();
+        topNavMenu.removeAttribute('inert');
     }
 }
+
 setupTopNav(media);
 
-btnOpen.addEventListener('click', openHamburger);
-btnClose.addEventListener('click', closeHamburger);
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
 
-media.addEventListener('change', function(e){
+media.addEventListener('change', function (e) {
     setupTopNav(e);
 });
